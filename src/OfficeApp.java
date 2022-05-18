6052770b1c7959cbf8b005f75776f2ce318616e7
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2022-05-19 01:42:22 +0800

    feat: Add mode option and console mode

--- src/OfficeApp.java
+++ src/OfficeApp.java
@@ -1,5 +1,82 @@
+import java.util.Scanner;
+
 public class OfficeApp {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    int choiceOfMode = OfficeApp.modeSelectionPrompt();
+
+    switch (choiceOfMode) {
+      case 0:
+        System.out.println("Closing");
+        return;
+
+      default:
+      case 1:
+        OfficeApp.guiMode();
+        break;
+
+      case 2:
+        OfficeApp.consoleMode();
+        break;
+    }
+
   }
+
+  private static void guiMode() {
+  }
+
+  private static void consoleMode() {
+    SmallOffice smallOffice = new SmallOffice();
+    MediumOffice mediumOffice = new MediumOffice();
+    BigOffice bigOffice = new BigOffice();
+
+    LowRankingStaff low1 = new LowRankingStaff("John", "Developer", smallOffice);
+    LowRankingStaff low2 = new LowRankingStaff("Peter", "Designer");
+    LowRankingStaff low3 = new LowRankingStaff("Dickson", "System Analyst");
+
+    HighRankingStaff high1 = new HighRankingStaff("Davidson", "Project Manager", mediumOffice);
+    HighRankingStaff high2 = new HighRankingStaff("Davidson", "Tech Lead");
+
+    smallOffice.displayOccupants();
+    mediumOffice.displayOccupants();
+    bigOffice.displayOccupants();
+    System.out.println("");
+
+    smallOffice.addOccupant(low2);
+    mediumOffice.addOccupant(low3);
+
+    smallOffice.displayOccupants();
+    mediumOffice.displayOccupants();
+    bigOffice.displayOccupants();
+    System.out.println("");
+
+    bigOffice.addOccupant(low2);
+    bigOffice.addOccupant(low3);
+    bigOffice.addOccupant(high2);
+
+    smallOffice.displayOccupants();
+    mediumOffice.displayOccupants();
+    bigOffice.displayOccupants();
+  }
+
+  private static int modeSelectionPrompt() {
+    Scanner sc = new Scanner(System.in);
+    int answer = Integer.MIN_VALUE;
+
+    while (answer == Integer.MIN_VALUE) {
+      System.out.println("Please enter number of intended mode.");
+      System.out.println("1. GUI");
+      System.out.println("2. Console");
+      System.out.println("0. Exit");
+      int choice = sc.nextInt();
+      if (choice >= 0 && choice <= 2) {
+        answer = choice;
+      } else {
+        System.out.println("Invalid Choice.");
+      }
+    }
+
+    sc.close();
+    return answer;
+  }
+
 }
